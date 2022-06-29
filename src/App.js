1b4482e74acf9897126762e1518ea0effd48123a
Ashish Palsingh <ashish.codeappetite@gmail.com>
2022-06-28 21:44:34 -0400

    added firstname and lastname to the form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import ReactDOM from "react-dom/client";
+
+function UseForm() {
+  const [name, setName] = useState("");
+  const [lname, setLname] = useState("");
+  return (
+    <form>
+      <label>
+        Enter Firstname:
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        ></input>
+      </label>
+      <label>
+        Enter Lastname:
+        <input
+          type="text"
+          value={lname}
+          onChange={(e) => setLname(e.target.value)}
+        ></input>
+      </label>
+    </form>
+  );
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  root.render(<UseForm />);
 }
 
 export default App;
